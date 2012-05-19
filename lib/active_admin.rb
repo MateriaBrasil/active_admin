653c9343fb23b7f33be2bf8a517a57f84e77e576
Luiz Fonseca <runeroniek@gmail.com>
2012-05-19 18:14:51 -0300

    Fix filter.

--- lib/active_admin.rb
+++ lib/active_admin.rb
@@ -27,7 +27,7 @@ module ActiveAdmin
   autoload :DSL,                      'active_admin/dsl'
   autoload :Event,                    'active_admin/event'
   autoload :FormBuilder,              'active_admin/form_builder'
-  autoload :FilterFormBuilder,        'active_admin/filter_form_builder'
+  #autoload :FilterFormBuilder,        'active_admin/filter_form_builder'
   autoload :Inputs,                   'active_admin/inputs'
   autoload :DslFormBuilder,           'active_admin/dsl_form_builder'
   autoload :Iconic,                   'active_admin/iconic'
