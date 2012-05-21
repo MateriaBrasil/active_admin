ed346e99c78a5816ce3219e7f2194fd5b62f6b34
Luiz Fonseca <runeroniek@gmail.com>
2012-05-20 23:32:04 -0300

    Fix undefined method.

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -8,35 +8,38 @@ module ActiveAdmin
 
       # Add Delete Links
       form_block = proc do |has_many_form|
-        block.call(has_many_form) + if has_many_form.object.new_record?
-                                      template.content_tag :li do
-                                        template.link_to I18n.t('active_admin.has_many_delete'), "#", :onclick => "$(this).closest('.has_many_fields').remove(); return false;", :class => "button"
-                                      end
-                                    else
-                                    end
+        fields = template.capture(has_many_form, &block)
+
+        onclick = if has_many_form.object.new_record?
+                    "$(this).closest('.has_many_fields').remove(); return false;"
+                  else
+                    "$(this).closest('.has_many_fields').slideUp().find('.destroy').val('1'); return false;"
+                  end
+
+        delete_button = template.content_tag :li do
+          template.link_to I18n.t('active_admin.has_many_delete'), "#", :onclick => onclick, :class => "button"
+        end
+
+        fields + delete_button
       end
 
-      content = with_new_form_buffer do
-        template.content_tag :div, :class => "has_many #{association}" do
-          form_buffers.last << template.content_tag(:h3, association.to_s.titlecase)
-          inputs options, &form_block
+      template.content_tag :div, :class => "has_many #{association}" do
+        buffer = "".html_safe
+        buffer << template.content_tag(:h3, association.to_s.titlecase)
+        buffer << inputs(options, &form_block)
 
-          # Capture the ADD JS
-          js = with_new_form_buffer do
-            inputs_for_nested_attributes  :for => [association, object.class.reflect_on_association(association).klass.new],
-                                          :class => "inputs has_many_fields",
-                                          :for_options => {
-                                            :child_index => "NEW_RECORD"
-                                          }, &form_block
-          end
+        # Capture the ADD JS
+        js = inputs_for_nested_attributes :for => [association, object.class.reflect_on_association(association).klass.new],
+          :class => "inputs has_many_fields",
+          :for_options => {
+            :child_index => "NEW_RECORD"
+          }, &form_block
 
           js = template.escape_javascript(js)
           js = template.link_to I18n.t('active_admin.has_many_new', :model => association.to_s.singularize.titlecase), "#", :onclick => "$(this).before('#{js}'.replace(/NEW_RECORD/g, new Date().getTime())); return false;", :class => "button"
 
-          form_buffers.last << js.html_safe
-        end
+          buffer << js
       end
-      form_buffers.last << content.html_safe
     end
   end
 end
